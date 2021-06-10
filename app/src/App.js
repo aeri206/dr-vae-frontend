13ed3484f367f9bdb25494f7c51e92e80e5c2aee
aeri206 <aeri33590@gmail.com>
2021-06-10 11:38:25 +0900

    React Initial Commit

---
+++ app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        VAE for Dimensionality Reduction Explorer
+      </header>
+      <div id="body">
+      </div>
+      <footer className="App-footer">
+        Human-Computer Interaction 2021 Sprint Team 4
+      </footer>
+    </div>
+  );
+}
+
+export default App;
